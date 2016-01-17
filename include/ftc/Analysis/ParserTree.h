dc02e4965c53669f062a48bd197571bc7df5a830
Yukio Siraichi <sir.yukio@gmail.com>
2016-01-17 15:40:27 -0200

    Structure changed.

--- include/ftc/Analysis/ParserTree.h
+++ include/ftc/Analysis/ParserTree.h
@@ -1,59 +1,110 @@
 #ifndef PARSERTREE_H
 #define PARSERTREE_H
 
+typedef struct ExprT ExprT;
+typedef struct LitT LitT;
+typedef struct ArrayAccessT ArrayAccessT;
+typedef struct RecordAccessT RecordAccessT;
+typedef struct LvalT LvalT;
+typedef struct BinOpT BinOpT;
+typedef struct LetT LetT;
+typedef struct DeclT DeclT;
+typedef struct TypeT TypeT;
+typedef struct ParamTyT ParamTyT;
+typedef struct VarDeclT VarDeclT;
+typedef struct FunDeclT FunDeclT;
+typedef struct ArrayT ArrayT;
+typedef struct FunTyT FunTyT;
+typedef struct TyDeclT TyDeclT;
+typedef struct IfStmtT IfStmtT;
+typedef struct FunCallT FunCallT;
+typedef struct ArgT ArgT;
+typedef struct CreateT CreateT;
+typedef struct ArrayTyT ArrayTyT;
+typedef struct RecordT RecordT;
+typedef struct FieldT FieldT;
+
 // -----------------------------------------------= Function Call
 struct FieldT {
   char   *Id;
   ExprT  *Expr;
-  FieldT *Next
+  FieldT *Next;
+  TypeT *Type;
 };
 
 struct RecordT {
   char   *Id;
   FieldT *Field;
+  TypeT *Type;
 };
 
 struct ArrayT {
   char  *Id;
   ExprT *Size;
   ExprT *Value;
+  TypeT *Type;
 };
 
 struct CreateT {
-  int Idx;
+  enum {
+    Record, Array 
+  } Idx;
   union {
     RecordT *Record;
     ArrayT  *Array;
   } Value;
+  TypeT *Type;
 };
 
 // -----------------------------------------------= Function Call
 struct ArgT {
   ExprT *Expr;
   ArgT  *Next;
+  TypeT *Type;
 };
 
 struct FunCallT {
   char *Id;
   ArgT *Args;
+  TypeT *Type;
 };
 
 // -----------------------------------------------= If-Then-Else
 struct IfStmtT {
+  TypeT *Type;
   ExprT *If;
   ExprT *Then;
   ExprT *Else;
 };
 
 // -----------------------------------------------= Declarations
+struct ParamTyT {
+  char  *Id;
+  TypeT *Type;
+  ParamTyT *Next;
+};
+
+struct TypeT {
+  enum { 
+    Preset, Custom 
+  } Idx;
+  union {
+    int  Type;
+    char *Id;
+  } Value;
+};
+
 // >> Type Declaration
 struct TyDeclT {
   char *Id;
+  enum {
+    Type, ParamTy, FunTy, ArrayTy
+  } Idx;
   union {
-    TypeT  *Type;
-    ParamT *Params;
-    FunTyT *Func;
-    ArrayT *Arry;
+    TypeT    *Type;
+    ParamTyT   *Params;
+    FunTyT   *Func;
+    ArrayTyT *ArrayTy;
   } Ty;
 };
 
@@ -62,7 +113,7 @@ struct FunTyT {
   TyDeclT *To;
 };
 
-struct ArrayT {
+struct ArrayTyT {
   TypeT Type;
 };
 
@@ -71,7 +122,7 @@ struct FunDeclT {
   char   *Id;
   TypeT  *Type;
   ExprT  *Expr;
-  ParamT *Params;
+  ParamTyT *Params;
   FunDeclT *Next;
 };
 
@@ -83,42 +134,40 @@ struct VarDeclT {
 };
 
 // >> Declaration
-struct ParamT {
-  char  *Id;
-  TypeT *Type;
-  ParamT *Next;
-};
-
-struct TypeT {
-  int  Type;
-  char *Id;
-};
-
 struct DeclT {
-  DeclT *Next;
+  enum {
+    Var, Fun, Ty
+  } Idx;
   union {
     VarDeclT *Var;
     FunDeclT *Fun;
     TyDeclT  *Type;
   } Value;
+  DeclT *Next;
 };
 
 // -----------------------------------------------= Let
 struct LetT {
+  TypeT *Type;
   DeclT *Decl;
   ExprT *Expr;
 };
 
 // -----------------------------------------------= Binary Operations
 struct BinOpT {
-  int Idx;
+  TypeT *Type;
+  enum {
+    Sum, Sub, Mult, Div
+  } Idx;
   ExprT *ExprOne;
   ExprT *ExprTwo;
 };
 
 // -----------------------------------------------= L-Values
 struct LvalT {
-  int Idx;
+  enum {
+    Id, RecordAccess, ArrayAccess
+  } Idx;
   union {
     char *Id; 
     RecordAccessT *Rec;
@@ -138,7 +187,9 @@ struct ArrayAccessT {
 
 // -----------------------------------------------= Literals
 struct LitT {
-  int Idx;
+  enum { 
+    Int, Float, String
+  } Idx;
   union {
     int    Int;
     float  Flt;
@@ -148,7 +199,10 @@ struct LitT {
 
 // -----------------------------------------------= Expression
 struct ExprT {
-  int Idx;
+  TypeT *Type;
+  enum {
+    Lit, Lval, BinOp, Let, IfStmt, FunCall, Create
+  } Idx;
   union {
     LitT     *Lit;
     LvalT    *Lval;
