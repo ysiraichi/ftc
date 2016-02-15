10d1f16dd06f9f33501ef4949d2d7b25e1d5a300
Yukio Siraichi <sir.yukio@gmail.com>
2016-02-15 19:55:14 -0200

    Main: Command line args and help message added.

--- src/Main.c
+++ src/Main.c
@@ -2,9 +2,12 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "llvm-c/Core.h"
 #include "llvm-c/Target.h"
+#include "llvm-c/TargetMachine.h"
+#include "llvm-c/Initialization.h"
 
 #include "ftc/Analysis/ParserTree.h"
 #include "ftc/Analysis/DrawTree.h"
@@ -14,11 +17,18 @@
 #include "ftc/LLVMCode/Translate.h"
 #include "ftc/LLVMCode/BaseCode.h"
 
+typedef enum {
+  NONE, OUTPUT, HELP
+} CommandArgsOpt;
+
 extern FILE    *yyin;
 extern ASTNode *Root;
 
-static char *OutputFileName;
-static char *TreeFileName;
+static char OutputFileName[128];
+
+static int PrintLLVM = 0;
+static int PrintAssembly = 0;
+static int PrintASTree = 0;
 
 static char MainFName[] = "main";
 static char MainFEscaped[] = "escaped.main";
@@ -40,7 +50,7 @@ void initLLVM() {
   LLVMPositionBuilderAtEnd(Builder, Entry);
 }
 
-void doCompile() {
+void doAnalysisToLLVMIR() {
   ASTNode     *MainFunc = createASTNode(FunDecl, MainFName, 0);
   SymbolTable *BaseTy   = createSymbolTable(NULL, 0),
               *BaseVal  = createSymbolTable(NULL, 0);
@@ -82,27 +92,58 @@ void doCompile() {
   LLVMBuildRet(Builder, getSConstInt(0));
 }
 
+void doCompileLLVMIRtoAssembly() {
+  /*
+     LLVMPassRegistryRef Registry = LLVMGetGlobalPassRegistry();
+     LLVMInitializeCore(Registry);
+     */
+  LLVMPassRegistryRef Registry = LLVMGetGlobalPassRegistry();
+  LLVMInitializeCore(Registry);
+  LLVMInitializeCodeGen(Registry);
+  LLVMInitializeAllTargetInfos();
+  LLVMInitializeAllTargets();
+  LLVMInitializeAllTargetMCs();
+  LLVMInitializeAllAsmPrinters();
+  LLVMInitializeAllAsmParsers();
+
+  char *Triple, *Error, CPU[] = "generic", Features[] = "", OutputAssembly[128];
+  LLVMBool Result;
+  LLVMTargetRef Target;
+  LLVMTargetMachineRef TargetMachine;
+
+  strcpy(OutputAssembly, OutputFileName);
+  strcat(OutputAssembly, ".s");
+
+  Triple = LLVMGetDefaultTargetTriple();
+  LLVMGetTargetFromTriple(Triple, &Target, &Error);
+  if (!Target) printf("Error: %s\n", Error);
+  TargetMachine = LLVMCreateTargetMachine(Target, Triple, CPU, Features, 
+      LLVMCodeGenLevelNone, LLVMRelocDefault, LLVMCodeModelDefault);
+
+  Result = LLVMTargetMachineEmitToFile(TargetMachine, Module, OutputAssembly, LLVMAssemblyFile, &Error);
+  if (Result) printf("Error: %s\n", Error);
+
+}
+
 void printHelp() {
   printf("\n-------------------- The Pure-Functional Tiger Language Compiler --------------------------\n\n");
-  printf("> Usage: ftc <inputFile> [-o <outputFile>] [-tree <treeOutputFile>]\n\n");
+  printf("> Usage: ftc <inputFile> [-o <outputFile>] [-tree] [-emit-llvm ] [-emit-as]\n\n");
 
   printf("> Arguments:\n");
-  printf("    . InputFile :: the file which contains a pure-functional tiger program;\n");
-  printf("    . -o        :: the output file in which will contain the LLVM IR;\n");
-  printf("    . -tree     :: the tree output file in which will contain the AST in 'dot' format;\n");
+  printf("    . InputFile  :: the file which contains a pure-functional tiger program;\n");
+  printf("    . -o         :: the output file base name;\n");
+  printf("    . -tree      :: prints the program's ASTree in a 'dot' file graph;\n");
+  printf("    . -emit-llvm :: emits a file corresponding to the program in LLVM IR language;\n");
+  printf("    . -emit-as   :: emits a file corresponding to the program in Assembly language;\n");
   printf("\n-------------------------------------------------------------------------------------------\n\n");
   exit(0);
 }
 
-typedef enum {
-  NONE, OUTPUT, TREEPATH, HELP
-} CommandArgsOpt;
-
 void commandArgsParse(int argc, char **argv) {
   if (argc == 1) printHelp();
 
-  yyin           = NULL;
-  OutputFileName = NULL;
+  yyin = NULL;
+  OutputFileName[0] = '\0';
 
   int I;
   CommandArgsOpt O = NONE;
@@ -111,20 +152,16 @@ void commandArgsParse(int argc, char **argv) {
       case NONE:
         {
           if (!strcmp("-o", argv[I])) O = OUTPUT;
-          else if (!strcmp("-tree", argv[I])) O = TREEPATH;
           else if (!strcmp("-h", argv[I]) || !strcmp("--help", argv[I])) O = HELP;
+          else if (!strcmp("-tree", argv[I])) PrintASTree = 1;
+          else if (!strcmp("-emit-llvm", argv[I])) PrintLLVM = 1;
+          else if (!strcmp("-emit-as", argv[I])) PrintAssembly = 1;
           else yyin = fopen(argv[I], "r");
           break;
         }
       case OUTPUT:
         {
-          OutputFileName = argv[I];
-          O = NONE;
-          break;
-        }
-      case TREEPATH:
-        {
-          TreeFileName = argv[I];
+          strcpy(OutputFileName, argv[I]);
           O = NONE;
           break;
         }
@@ -140,24 +177,35 @@ void commandArgsParse(int argc, char **argv) {
   if (O != NONE) printHelp();
 
   if (!yyin) printHelp();
-  if (!OutputFileName) {
-    OutputFileName = (char*) malloc(sizeof(char) * 12);
-    strcpy(OutputFileName, "/dev/stderr");
-    OutputFileName[11] = '\0';
-  }
-  if (!TreeFileName) {
-    TreeFileName = (char*) malloc(sizeof(char) * 12);
-    strcpy(TreeFileName, "/dev/stdout");
-    TreeFileName[11] = '\0';
+  if (!OutputFileName[0]) {
+    strcpy(OutputFileName, "a");
+    OutputFileName[2] = '\0';
   }
 }
 
 int main(int argc, char **argv) {
   commandArgsParse(argc, argv);
   if (!yyparse()) {
-    drawDotTree(TreeFileName, Root);
-    doCompile();
-    LLVMPrintModuleToFile(Module, OutputFileName, NULL);
+    if (PrintASTree) {
+      char TreeFileName[128];
+      sprintf(TreeFileName, "%s.dot", OutputFileName);
+      drawDotTree(TreeFileName, Root);
+    }
+
+    doAnalysisToLLVMIR();
+
+    char LLVMIRFile[128], ExeFile[128], Command[256];
+    if (!strcmp(OutputFileName, "a")) 
+      sprintf(ExeFile, "%s.out", OutputFileName);
+    else strcpy(ExeFile, OutputFileName);
+    sprintf(LLVMIRFile, "%s.ll", OutputFileName);
+
+    LLVMPrintModuleToFile(Module, LLVMIRFile, NULL);
+    sprintf(Command, "clang-3.7 %s -o %s", LLVMIRFile, ExeFile);
+    system(Command); 
+
+    if (PrintAssembly) doCompileLLVMIRtoAssembly();
+    if (!PrintLLVM)    remove(LLVMIRFile);
     destroyASTNode(Root);
     return 0;
   }
